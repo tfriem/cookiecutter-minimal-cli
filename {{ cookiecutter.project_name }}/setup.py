ef74d7e7beb2572da48d0d2dbe08d1e230e2df89
Tobias Friemel <tfriem@gmail.com>
2018-04-10 19:38:07 +0200

    Initial commit

---
+++ {{ cookiecutter.project_name }}/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='{{ cookiecutter.project_slug }}',
+      version='0.1',
+      author='{{ cookiecutter.author }}',
+      author_email='{{ cookiecutter.author_email }}',
+      packages=['{{ cookiecutter.project_slug }}'],
+      entry_points={
+          'console_scripts': ['{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.command_line:main']
+      })
