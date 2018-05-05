5f5bce532b13d8d5dd92397204aad523dfd510c9
Tobias Friemel <tfriem@gmail.com>
2018-05-05 20:23:29 +0200

    Add install_requires entry to setup.py template

--- {{ cookiecutter.project_name }}/setup.py
+++ {{ cookiecutter.project_name }}/setup.py
@@ -5,6 +5,7 @@ setup(name='{{ cookiecutter.project_slug }}',
       author='{{ cookiecutter.author }}',
       author_email='{{ cookiecutter.author_email }}',
       packages=['{{ cookiecutter.project_slug }}'],
+      install_requires=[],
       entry_points={
           'console_scripts': ['{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.command_line:main']
       })
